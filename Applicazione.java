e2f76e05142708c8322abc9584f55c8784143bfb
Federicob7 <bissantefederico@gmail.com>
2025-01-22 09:04:34 +0100

    F3

--- Applicazione.java
+++ Applicazione.java
@@ -3,14 +3,20 @@ import java.awt.*;
 
 public class Applicazione {
     public static void main(String[] args) {
-        F1 nuova = new F1(null);
-        nuova.setVisible(true);
-        nuova.setLocation(300,400);
-        nuova.setSize(null);
+        //F1 nuova = new F1(null);
+        // nuova.setVisible(true);
+        //nuova.setLocation(300,400);
+        //nuova.setSize(null);
 
-        F2 nuove = new F2(null);
+        //F2 nuove = new F2(null);
         //nuove.setVisible(true);
         //nuove.setLocation(300,400);
         //nuove.setSize(null);
+        
+        F3 nuovi =new F3(null);
+        nuovi.setVisible(true);
+        nuovi.setLocation(300,200);
+        nuovi.setSize(null);
+    
     }
 }
